a9b8814237ab8eecad7dbc25e6b92548e35aeaa7
Christoph Lipautz <christoph@lipautz.org>
2012-11-12 21:52:35 +0100

    adding basic input controls (windows not supported)

--- lib/airstream/io.rb
+++ lib/airstream/io.rb
@@ -1,7 +1,10 @@
 
+require 'io/wait'
+
 module Airstream
   class Io
     attr_writer :quiet, :verbose
+    attr_reader :key
 
     def print(msg)
       STDOUT.print msg unless @quiet
@@ -15,5 +18,29 @@ module Airstream
       STDOUT.puts msg if @verbose
     end
 
+    def hide_input
+      `stty raw -echo`
+    end
+
+    def show_input
+      `stty -raw echo`
+    end
+
+    def catch_input
+      @key = nil
+      if STDIN.ready?
+        @key = STDIN.getc
+      end
+    rescue
+      show_input
+    end
+
+    def quit?
+      @key == "q"
+    end
+
+    def skip?
+      @key == "s"
+    end
   end
 end
