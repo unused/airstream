fca2302f3fe4af9acc0e40fc72ca99336253da74
Pascal Borreli <pascal@borreli.com>
2014-07-28 04:18:29 +0000

    Fixed typos

--- lib/airstream/node.rb
+++ lib/airstream/node.rb
@@ -2,9 +2,9 @@
 module Airstream
   class Node < Airplay::Client
 
-    def initialize(reciever)
+    def initialize(receiver)
       node = Airplay::Server::Node.new(
-        reciever, reciever, reciever, AIRPLAY_PORT)
+        receiver, receiver, receiver, AIRPLAY_PORT)
       super node
       # TODO test if does not work without
       use node # does not work without that second assign
--- test/device_test.rb
+++ test/device_test.rb
@@ -3,10 +3,10 @@ require File.expand_path('helper/bootstrap', File.dirname(__FILE__))
 describe Airstream::Device do
 
   describe "when a video file is set" do
-    it "a title shoud be read from filename" do
-      reciever = MiniTest::Mock.new
-      reciever.expect :send_video, true, ["http://example.com/my-birthday-2010.mp4"]
-      device = Airstream::Device.new reciever
+    it "a title should be read from filename" do
+      receiver = MiniTest::Mock.new
+      receiver.expect :send_video, true, ["http://example.com/my-birthday-2010.mp4"]
+      device = Airstream::Device.new receiver
       video = MiniTest::Mock.new
       video.expect :url, "http://example.com/my-birthday-2010.mp4"
       video.expect :to_s, "my-birthday-2010"
